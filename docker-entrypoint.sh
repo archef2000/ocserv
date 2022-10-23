a574d76338c0ad13f4d263902dab28b39841c6ab
Archef2000 <69057532+Archef2000@users.noreply.github.com>
2022-10-23 21:39:46 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -94,7 +94,7 @@ generate_cert(){
         cert_signing_key
         crl_signing_key
 EOCA
-        certtool --generate-self-signed --load-privkey $cert_dir/ca-key.pem --template /tmp/ca.tmpl --outfile $cert_dir/ca.pem
+        certtool --generate-self-signed --load-privkey $cert_dir/ca-key.pem --template /tmp/ca.tmpl --outfile $cert_dir/ca-cert.pem
         certtool --generate-privkey --outfile $server_key_path
         cat > /tmp/server.tmpl <<-EOSRV
         cn = "$DOMAIN"
@@ -104,7 +104,7 @@ EOCA
         encryption_key
         tls_www_server
 EOSRV
-        certtool --generate-certificate --load-privkey $server_key_path --load-ca-certificate $cert_dir/ca.pem \
+        certtool --generate-certificate --load-privkey $server_key_path --load-ca-certificate $cert_dir/ca-cert.pem \
 	--load-ca-privkey $cert_dir/ca-key.pem --template /tmp/server.tmpl --outfile $server_cert_path
     fi
 }
